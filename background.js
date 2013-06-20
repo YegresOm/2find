92ff17b273abd823832afad885efb5e473808b66
Sergii Motynga <yegresom@gmail.com>
2013-06-20 03:37:10 +0200

    minor fix

--- background.js
+++ background.js
@@ -2,12 +2,10 @@ var version = new Date().getMilliseconds(),
     folderId = null,
 
     addBookmark = function (song) {
-        console.log("song name: " + song);
         if (song.length !== 0) {
-
             chrome.bookmarks.create({
                 parentId: folderId,
-                title: song + version,
+                title: song,
                 url: "https://www.google.com/search?btnG=1&pws=0&q=" + song
             }, function (bookmark) {
                 console.log(bookmark);
@@ -17,7 +15,6 @@ var version = new Date().getMilliseconds(),
 
     requestSaveAsBookmark = function (tabId) {
         chrome.tabs.sendMessage(tabId, {action: "getSongName"}, function (response) {
-            console.log(response.songName.trim().length + "_V_" + version);
             addBookmark(response.songName.trim());
         });
     },
@@ -25,7 +22,6 @@ var version = new Date().getMilliseconds(),
     findPage = function (tabs) {
         $(tabs).each(function (index) {
             if (tabs[index].url.indexOf('radiotuna') !== -1) {
-                console.log(tabs[index].url + "_v_" + version);
                 requestSaveAsBookmark(tabs[0].id);
             }
         });
@@ -35,7 +31,6 @@ chrome.bookmarks.getChildren("2", function (bookmarks) {
     var notExists = true
     $(bookmarks).each(function (index) {
         if (bookmarks[index].title.match('2Find')) {
-            console.log("Parrent " + bookmarks[index].title)
             folderId = bookmarks[index].id
             notExists = false;
         }
@@ -44,7 +39,6 @@ chrome.bookmarks.getChildren("2", function (bookmarks) {
         chrome.bookmarks.create({'parentId': null,
                 'title': '2Find'},
             function (newFolder) {
-                console.log("Folder added");
                 folderId = newFolder.id;
             });
     }
