fce702299480f978cc4556b96b8131739c2d0563
Sergii Motynga <yegresom@gmail.com>
2013-06-20 03:32:30 +0200

    implementrd bookmarks add

--- background.js
+++ background.js
@@ -1,17 +1,60 @@
-var saveSongAsBookmark = function (tabId) {
-    chrome.tabs.sendMessage(tabId, {action: "getSongName"}, function (response) {
-        alert(response.songName);
-    });
-}
+var version = new Date().getMilliseconds(),
+    folderId = null,
 
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.msg == 'run') {
-            chrome.tabs.getAllInWindow(null, function (tabs) {
-                saveSongAsBookmark(tabs[0].id);
+    addBookmark = function (song) {
+        console.log("song name: " + song);
+        if (song.length !== 0) {
 
-                //	if(tabs[index].url.indexOf("radiotuna.com") !== -1){
-                //	}
-            })
+            chrome.bookmarks.create({
+                parentId: folderId,
+                title: song + version,
+                url: "https://www.google.com/search?btnG=1&pws=0&q=" + song
+            }, function (bookmark) {
+                console.log(bookmark);
+            });
         }
-    });
+    },
+
+    requestSaveAsBookmark = function (tabId) {
+        chrome.tabs.sendMessage(tabId, {action: "getSongName"}, function (response) {
+            console.log(response.songName.trim().length + "_V_" + version);
+            addBookmark(response.songName.trim());
+        });
+    },
+
+    findPage = function (tabs) {
+        $(tabs).each(function (index) {
+            if (tabs[index].url.indexOf('radiotuna') !== -1) {
+                console.log(tabs[index].url + "_v_" + version);
+                requestSaveAsBookmark(tabs[0].id);
+            }
+        });
+    }
+
+chrome.bookmarks.getChildren("2", function (bookmarks) {
+    var notExists = true
+    $(bookmarks).each(function (index) {
+        if (bookmarks[index].title.match('2Find')) {
+            console.log("Parrent " + bookmarks[index].title)
+            folderId = bookmarks[index].id
+            notExists = false;
+        }
+    })
+    if (notExists) {
+        chrome.bookmarks.create({'parentId': null,
+                'title': '2Find'},
+            function (newFolder) {
+                console.log("Folder added");
+                folderId = newFolder.id;
+            });
+    }
+});
+
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.msg == 'run') {
+        chrome.tabs.getAllInWindow(null, function (tabs) {
+            findPage(tabs);
+        })
+    }
+});
